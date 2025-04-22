59fc0d6d6c9e00715996688db1ef0309a3ce7b86
c0a240021c <c0a240021c@edu.teu.ac.jp>
2025-04-22 14:24:27 +0900

    練習2:爆弾の表示

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,6 +21,19 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    #練習2:爆弾の表示
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img, (255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    #練習2:爆弾rctの座標
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    #練習2:爆弾の移動
+    vx = +5
+    vy = +5
+
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -38,7 +52,13 @@ def main():
 
         
         kk_rct.move_ip(sum_mv)
+        #練習2:爆弾の移動
+        bb_rct.move_ip(vx,vy)
+
         screen.blit(kk_img, kk_rct)
+
+        #練習2:爆弾の表示
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
